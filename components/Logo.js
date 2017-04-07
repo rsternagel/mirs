a3668c72343f9a971738075da731baddd67b604f
Richard Sternagel <rsternagel@gmail.com>
2017-04-07 18:09:26 +0200

    add first logo iteration

--- components/Logo.js
+++ components/Logo.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
-import { rhythm, scale } from 'utils/typography'
+import logoMirs from './img/logo_mirs.png'
 
 class Logo extends React.Component {
   render () {
 
     return (
-      <div className="root">
-        <Link to={prefixLink('/')}>Logo</Link>
+      <div className="logo">
+        <Link to={prefixLink('/')}>
+          <img src={prefixLink(logoMirs)} alt="Medieninformatik Richard Sternagel" />
+        </Link>
 
          <style jsx>{`
-           .root :global(a) {
-            box-shadow: none;
-            color: inherit;
-            flex: 1;
+           /* TODO: replace with smaller img */
+           .logo :global(img) {
+             margin-left: -10px;
+             margin-top: 5px;
+             width: 70%;
            }
          `}</style>
       </div>
