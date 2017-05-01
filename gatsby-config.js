a313791f200d2f23a500a790b189c5ab0bdfdf41
Richard Sternagel <rsternagel@gmail.com>
2017-05-01 12:58:49 +0200

    insert trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-22466234-1',
       },
     },
     `gatsby-plugin-offline`,
