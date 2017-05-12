0ebb60e3148edabb2f1cec0fedee1d6959852d71
Richard Sternagel <rsternagel@gmail.com>
2017-05-12 15:05:39 +0200

    add impressum page

---
+++ src/pages/impressum.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+class Impressum extends React.Component {
+  render() {
+    const site = this.props.data.site
+    const siteTitle = site.siteMetadata.title
+    const domain = site.siteMetadata.domain
+
+    return (
+      <div>
+        <Helmet title={`Impressum | ${siteTitle}`}/>
+
+        <h1>Impressum</h1>
+        <p>
+          Verantwortlich gemäß § 5 TMG:
+        </p>
+
+        <h2>Anschrift</h2>
+        <p>
+          Medieninformatik<br/>
+          Richard Sternagel<br/>
+          Diplom Medien-Informatiker (DH)<br/>
+          Geleitsstraße 17<br/>
+          60599 Frankfurt
+        </p>
+        <h2>Kontakt</h2>
+        <p>
+          <span>Telefon:</span> +49 176 / 217 466 40<br/>
+          <span>E-Mail:</span> rsternagel@posteo.de<br/>
+        </p>
+        <h2>Umsatzsteuer-ID</h2>
+        <p>
+          DE310204622
+        </p>
+
+        <style jsx>{`
+          span {
+            color: #555;
+            padding: 0 3px 0 0;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+
+export default Impressum
+
+export const pageQuery = `
+ {
+   site {
+     siteMetadata {
+       title
+       domain
+     }
+   }
+ }
+`
