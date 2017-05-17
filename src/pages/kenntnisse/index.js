6f5ae618a36662d88a47014ffc0b78478ca272b2
Richard Sternagel <rsternagel@gmail.com>
2017-05-17 09:32:12 +0200

    switch to stateless functional components - part III

--- src/pages/kenntnisse/index.js
+++ src/pages/kenntnisse/index.js
@@ -8,91 +8,86 @@ import Technologies from '../../components/skills/Technologies'
 import Tools from '../../components/skills/Tools'
 import MiscSkills from '../../components/skills/MiscSkills'
 
-class Skills extends React.Component {
-  render () {
-    const site = this.props.data.site
-    const siteTitle = site.siteMetadata.title
+const Skills = ({
+  data: {site: {siteMetadata: {title: siteTitle}}}
+}) => (
+  <div>
 
-    return (
-      <div>
+    <Helmet title={`Kenntnisse | ${siteTitle}`}/>
 
-        <Helmet title={`Kenntnisse | ${siteTitle}`}/>
+    <h1>Kenntnisse</h1>
+    <p>Diese Liste spiegelt wider <span
+       className="low">woher ich komme</span>, <span
+       className="middle">was ich bevorzuge</span> und <span
+       className="high">wofür ich brenne</span>.
+    </p>
 
-        <h1>Kenntnisse</h1>
-        <p>Diese Liste spiegelt wider <span
-           className="low">woher ich komme</span>, <span
-           className="middle">was ich bevorzuge</span> und <span
-           className="high">wofür ich brenne</span>.
-        </p>
+    <div className="skills">
+      <MainSkills />
+      <Technologies />
+      <Tools />
+      <MiscSkills />
+    </div>
 
-        <div className="skills">
-          <MainSkills />
-          <Technologies />
-          <Tools />
-          <MiscSkills />
-        </div>
+    <style jsx>{`
+      .skills {
+        margin-bottom: 30px;
+      }
 
-        <style jsx>{`
-          .skills {
-            margin-bottom: 30px;
-          }
+      .skills :global(ul) {
+        list-style-type: none;
+      }
 
-          .skills :global(ul) {
-            list-style-type: none;
-          }
+      .skills :global(ul) {
+        list-style-type: none;
+      }
 
-          .skills :global(ul) {
-            list-style-type: none;
-          }
+      .skills :global(ul),
+      .skills :global(dl),
+      .skills :global(dd) {
+        margin-bottom: 0;
+      }
 
-          .skills :global(ul),
-          .skills :global(dl),
-          .skills :global(dd) {
-            margin-bottom: 0;
-          }
+      .skills :global(.skill-list) {
+        display: flex;
+      }
 
-          .skills :global(.skill-list) {
-            display: flex;
-          }
+      .skills :global(.category),
+      .skills :global(.details) {
+        padding: 5px 0 5px 0;
+      }
 
-          .skills :global(.category),
-          .skills :global(.details) {
-            padding: 5px 0 5px 0;
-          }
+      .skills :global(.category) {
+        width: 30%;
+        margin-right: 20px;
+      }
 
-          .skills :global(.category) {
-            width: 30%;
-            margin-right: 20px;
-          }
+      .skills :global(.details) {
+        width: 70%;
+      }
 
-          .skills :global(.details) {
-            width: 70%;
-          }
+      .skills :global(.row) {
+        display: flex;
+        flow-direction: row;
+      }
 
-          .skills :global(.row) {
-            display: flex;
-            flow-direction: row;
-          }
+      .skills :global(.details) :global(li) {
+        padding-left: 5px;
+        margin-bottom: 3px;
+      }
 
-          .skills :global(.details) :global(li) {
-            padding-left: 5px;
-            margin-bottom: 3px;
-          }
-
-          .skills :global(.low), .low {
-            color: hsla(220, 0%, 60%, .9);
-          }
-          .skills :global(.middle), .middle {
-            color: hsla(33, 40%, 35%, .9);
-          }
-          .skills :global(.high), .high {
-            color: hsla(130, 45%, 30%, 1);
-          }
-        `}</style>
-      </div>
-    )
-  }
-}
+      .skills :global(.low), .low {
+        color: hsla(220, 0%, 60%, .9);
+      }
+      .skills :global(.middle), .middle {
+        color: hsla(33, 40%, 35%, .9);
+      }
+      .skills :global(.high), .high {
+        color: hsla(130, 45%, 30%, 1);
+      }
+    `}</style>
+  </div>
+)
 
 Skills.propTypes = {
   route: PropTypes.object,
