d30c5b2d7988cc31b6e94a5525b3780d7f3662b2
Richard Sternagel <rsternagel@gmail.com>
2017-05-17 11:18:46 +0200

    update gatsby deps to alpha15

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -24,7 +24,7 @@ const Template = ({
         <Nav location={location} />
       </header>
       <section className="maincontent" role="main">
-        {children}
+        {children()}
       </section>
 
      <style jsx>{`
