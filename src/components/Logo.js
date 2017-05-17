ee4e2600d9f8e2f45c737f60c80ca3d68ec1c8b6
Richard Sternagel <rsternagel@gmail.com>
2017-05-17 03:07:58 +0200

    switch to stateless functional components - part II

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,27 +2,22 @@ import React from 'react'
 import Link from 'gatsby-link'
 import logoMirs from './img/logo_mirs.png'
 
-class Logo extends React.Component {
-  render () {
+const Logo = () => (
+  <div className="logo">
+    <Link to={'/'}>
+      <img src={logoMirs} alt="Medieninformatik Richard Sternagel" />
+    </Link>
 
-    return (
-      <div className="logo">
-        <Link to={'/'}>
-          <img src={logoMirs} alt="Medieninformatik Richard Sternagel" />
-        </Link>
-
-        <style jsx>{`
-          /* TODO: replace with smaller img */
-          .logo :global(img) {
-            margin-left: -10px;
-            margin-top: 5px;
-            width: 65%;
-          }
-        `}</style>
-      </div>
-    )
-  }
-}
+    <style jsx>{`
+      /* TODO: replace with smaller img */
+      .logo :global(img) {
+        margin-left: -10px;
+        margin-top: 5px;
+        width: 65%;
+      }
+    `}</style>
+  </div>
+)
 
 export default Logo
 
--- src/components/skills/Tools.js
+++ src/components/skills/Tools.js
@@ -1,120 +1,115 @@
 import React from 'react'
 
-class Tools extends React.Component {
-  render () {
+const Tools = () => (
+  <div>
+    <dl className="skill-list">
+      <dt className="category">Dev</dt>
+      <dd className="details">
+        <ul className="row">
+          <li className="high">(M)Vim,</li>
+          <li className="middle">Atom,</li>
+          <li className="low">Eclipse</li>
+        </ul>
+      </dd>
+    </dl>
 
-    return (
-      <div>
-        <dl className="skill-list">
-          <dt className="category">Dev</dt>
-          <dd className="details">
-            <ul className="row">
-              <li className="high">(M)Vim,</li>
-              <li className="middle">Atom,</li>
-              <li className="low">Eclipse</li>
-            </ul>
-          </dd>
-        </dl>
+    <dl className="skill-list">
+      <dt className="category">Testing</dt>
+      <dd className="details">
+        <ul className="row">
+          <li className="high">Jest,</li>
+          <li className="high">Enzyme,</li>
+          <li className="middle">Karma,</li>
+          <li className="middle">Jasmine,</li>
+          <li className="middle">Mocha,</li>
+          <li className="low">Protractor,</li>
+          <li className="low">Selenium</li>
+        </ul>
+      </dd>
+    </dl>
 
-        <dl className="skill-list">
-          <dt className="category">Testing</dt>
-          <dd className="details">
-            <ul className="row">
-              <li className="high">Jest,</li>
-              <li className="high">Enzyme,</li>
-              <li className="middle">Karma,</li>
-              <li className="middle">Jasmine,</li>
-              <li className="middle">Mocha,</li>
-              <li className="low">Protractor,</li>
-              <li className="low">Selenium</li>
-            </ul>
-          </dd>
-        </dl>
+    <dl className="skill-list">
+      <dt className="category">Qualitätskontrolle</dt>
+      <dd className="details">
+        <ul className="row">
+          <li className="high">eslint,</li>
+          <li className="high">prettier,</li>
+          <li className="high">flow,</li>
+          <li className="low">JSCS,</li>
+          <li className="low">JSHint,</li>
+          <li className="low">JSLint</li>
+        </ul>
+      </dd>
+    </dl>
 
-        <dl className="skill-list">
-          <dt className="category">Qualitätskontrolle</dt>
-          <dd className="details">
-            <ul className="row">
-              <li className="high">eslint,</li>
-              <li className="high">prettier,</li>
-              <li className="high">flow,</li>
-              <li className="low">JSCS,</li>
-              <li className="low">JSHint,</li>
-              <li className="low">JSLint</li>
-            </ul>
-          </dd>
-        </dl>
+    <dl className="skill-list">
+      <dt className="category">Dokumentation</dt>
+      <dd className="details">
+        <ul className="row">
+          <li className="high">Gatsby (JS),</li>
+          <li className="high">Sphinx (Python),</li>
+          <li className="middle">JSDoc,</li>
+          <li className="low">Maven Site</li>
+        </ul>
+      </dd>
+    </dl>
 
-        <dl className="skill-list">
-          <dt className="category">Dokumentation</dt>
-          <dd className="details">
-            <ul className="row">
-              <li className="high">Gatsby (JS),</li>
-              <li className="high">Sphinx (Python),</li>
-              <li className="middle">JSDoc,</li>
-              <li className="low">Maven Site</li>
-            </ul>
-          </dd>
-        </dl>
+    <dl className="skill-list">
+      <dt className="category">
+        <abbr title="Continuous Integration">CI</abbr>/
+        <abbr title="Continuous Delivery">CD</abbr>
+      </dt>
+      <dd className="details">
+        <ul className="row">
+          <li className="high">Travis CI,</li>
+          <li className="high">Greenkeeper,</li>
+          <li className="middle">Jenkins</li>
+        </ul>
+      </dd>
+    </dl>
 
-        <dl className="skill-list">
-          <dt className="category">
-            <abbr title="Continuous Integration">CI</abbr>/
-            <abbr title="Continuous Delivery">CD</abbr>
-          </dt>
-          <dd className="details">
-            <ul className="row">
-              <li className="high">Travis CI,</li>
-              <li className="high">Greenkeeper,</li>
-              <li className="middle">Jenkins</li>
-            </ul>
-          </dd>
-        </dl>
+    <dl className="skill-list">
+      <dt className="category"><abbr title="Distributed Version Control System">(D)VCS</abbr></dt>
+      <dd className="details">
+        <ul className="row">
+          <li className="high">Git,</li>
+          <li className="middle">Mercurial,</li>
+          <li className="low">Subversion</li>
+        </ul>
+      </dd>
+    </dl>
 
-        <dl className="skill-list">
-          <dt className="category"><abbr title="Distributed Version Control System">(D)VCS</abbr></dt>
-          <dd className="details">
-            <ul className="row">
-              <li className="high">Git,</li>
-              <li className="middle">Mercurial,</li>
-              <li className="low">Subversion</li>
-            </ul>
-          </dd>
-        </dl>
+    <dl className="skill-list">
+      <dt className="category">Build</dt>
+      <dd className="details">
+        <ul className="row">
+          <li className="high">npm,</li>
+          <li className="high">yarn,</li>
+          <li className="high">webpack,</li>
+          <li className="high">Babel,</li>
+          <li className="middle">Browserify,</li>
+          <li className="middle">Gulp,</li>
+          <li className="low">Grunt,</li>
+          <li className="low">Maven</li>
+        </ul>
+      </dd>
+    </dl>
 
-        <dl className="skill-list">
-          <dt className="category">Build</dt>
-          <dd className="details">
-            <ul className="row">
-              <li className="high">npm,</li>
-              <li className="high">yarn,</li>
-              <li className="high">webpack,</li>
-              <li className="high">Babel,</li>
-              <li className="middle">Browserify,</li>
-              <li className="middle">Gulp,</li>
-              <li className="low">Grunt,</li>
-              <li className="low">Maven</li>
-            </ul>
-          </dd>
-        </dl>
+    <dl className="skill-list">
+      <dt className="category">Project Management</dt>
+      <dd className="details">
+        <ul className="row">
+          <li className="high">GitHub,</li>
+          <li className="middle">JIRA,</li>
+          <li className="middle">Trello</li>
+        </ul>
+      </dd>
+    </dl>
 
-        <dl className="skill-list">
-          <dt className="category">Project Management</dt>
-          <dd className="details">
-            <ul className="row">
-              <li className="high">GitHub,</li>
-              <li className="middle">JIRA,</li>
-              <li className="middle">Trello</li>
-            </ul>
-          </dd>
-        </dl>
-
-        <style jsx>{`
-        `}</style>
-      </div>
-    )
-  }
-}
+    <style jsx>{`
+    `}</style>
+  </div>
+)
 
 export default Tools
 
