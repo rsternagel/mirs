f66dab871547e4036527bd37af3af829f0499e6e
Richard Sternagel <rsternagel@gmail.com>
2017-05-17 02:58:55 +0200

    switch to stateless functional components - part I

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -8,78 +8,75 @@ import Nav from '../components/Nav'
 import Logo from '../components/Logo'
 import Footer from '../components/Footer'
 
-class Template extends React.Component {
-  render () {
-    const { location, children } = this.props
+const Template = ({
+  location,
+  children
+}) => (
+  <div>
+    <Container
+      style={{
+        maxWidth: '51rem',
+        padding: '1.5rem 1.125rem',
+      }}
+    >
+      <header>
+        <Logo />
+        <Nav location={location} />
+      </header>
+      <section className="maincontent" role="main">
+        {children}
+      </section>
 
-    return (
-      <div>
-        <Container
-          style={{
-            maxWidth: '51rem',
-            padding: '1.5rem 1.125rem',
-          }}
-        >
-          <header>
-            <Logo />
-            <Nav location={this.props.location} />
-          </header>
-          <section className="maincontent" role="main">
-            {children}
-          </section>
+     <style jsx>{`
+       :global(body) {
+         border-top: 6px solid #6aae6a;
+         -webkit-font-smoothing: antialiased;
+         -moz-osx-font-smoothing: grayscale;
+       }
 
-         <style jsx>{`
-           :global(body) {
-             border-top: 6px solid #6aae6a;
-             -webkit-font-smoothing: antialiased;
-             -moz-osx-font-smoothing: grayscale;
-           }
+       header {
+         display: flex;
+         flex-direction: row;
+       }
 
-           header {
-             display: flex;
-             flex-direction: row;
-           }
+       .maincontent :global(h1) {
+         font-family: Palatino, 'Palatino Linotype', Georgia, serif;
+         font-size: 130%;
+         border-bottom: 2px solid #6aae6a;
+         display: inline-block;
+         margin-bottom: 30px;
+         padding: 0 5px;
+       }
 
-           .maincontent :global(h1) {
-             font-family: Palatino, 'Palatino Linotype', Georgia, serif;
-             font-size: 130%;
-             border-bottom: 2px solid #6aae6a;
-             display: inline-block;
-             margin-bottom: 30px;
-             padding: 0 5px;
-           }
+       .maincontent :global(h2) {
+         font-family: 'Open Sans', sans-serif;
+         font-size: 100%;
+         margin-bottom: .7rem;
+       }
 
-           .maincontent :global(h2) {
-             font-family: 'Open Sans', sans-serif;
-             font-size: 100%;
-             margin-bottom: .7rem;
-           }
+       .maincontent {
+         font-family: 'Open Sans', sans-serif;
+       }
 
-           .maincontent {
-             font-family: 'Open Sans', sans-serif;
-           }
+       .maincontent :global(a),
+       .maincontent :global(a:visited) {
+         text-decoration: none;
+         padding-bottom: 1px;
+         color: #1964ae;
+       }
 
-           .maincontent :global(a),
-           .maincontent :global(a:visited) {
-             text-decoration: none;
-             padding-bottom: 1px;
-             color: #1964ae;
-           }
+       .maincontent :global(a:hover) {
+         background-color: #ffb;
+       }
 
-           .maincontent :global(a:hover) {
-             background-color: #ffb;
-           }
-
-           .maincontent :global(ul) {
-             font-size: 95%;
-           }
-         `}</style>
-        </Container>
-        <Footer />
-      </div>
-    )
-  }
-}
+       .maincontent :global(ul) {
+         font-size: 95%;
+       }
+     `}</style>
+    </Container>
+    <Footer />
+  </div>
+);
 
 Template.propTypes = {
   children: PropTypes.any,
