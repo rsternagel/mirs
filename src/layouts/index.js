8e67ca7481f53af8ae5b534777de339e5da2af03
Richard Sternagel <rsternagel@gmail.com>
2017-05-19 12:17:43 +0200

    supersede react-responsive-grid

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Container } from 'react-responsive-grid'
 
 import 'sanitize.css/sanitize.css';
 
@@ -13,68 +12,70 @@ const Template = ({
   children
 }) => (
   <div>
-    <Container
-      style={{
-        maxWidth: '51rem',
-        padding: '1.5rem 1.125rem',
-      }}
-    >
-      <header>
+    <div className="container">
+      <header className="masthead">
         <Logo />
         <Nav location={location} />
       </header>
       <section className="maincontent" role="main">
         {children()}
       </section>
-
-     <style jsx>{`
-       :global(body) {
-         border-top: 6px solid #6aae6a;
-         -webkit-font-smoothing: antialiased;
-         -moz-osx-font-smoothing: grayscale;
-       }
-
-       header {
-         display: flex;
-         flex-direction: row;
-       }
-
-       .maincontent :global(h1) {
-         font-family: Palatino, 'Palatino Linotype', Georgia, serif;
-         font-size: 130%;
-         border-bottom: 2px solid #6aae6a;
-         display: inline-block;
-         margin-bottom: 30px;
-         padding: 0 5px;
-       }
-
-       .maincontent :global(h2) {
-         font-family: 'Open Sans', sans-serif;
-         font-size: 100%;
-         margin-bottom: .7rem;
-       }
-
-       .maincontent {
-         font-family: 'Open Sans', sans-serif;
-       }
-
-       .maincontent :global(a),
-       .maincontent :global(a:visited) {
-         text-decoration: none;
-         padding-bottom: 1px;
-         color: #1964ae;
-       }
-
-       .maincontent :global(a:hover) {
-         background-color: #ffb;
-       }
-
-       .maincontent :global(ul) {
-         font-size: 95%;
-       }
-     `}</style>
-    </Container>
+    </div>
     <Footer />
+
+    <style jsx>{`
+      :global(body) {
+        border-top: 6px solid #6aae6a;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+
+      .container {
+        max-width: 54rem;
+        margin: 0 auto;
+        padding: 1.5rem 2rem;
+      }
+
+      .masthead {
+        display: flex;
+        flex-direction: row;
+      }
+
+      .maincontent :global(h1) {
+        font-family: Palatino, 'Palatino Linotype', Georgia, serif;
+        font-size: 130%;
+        border-bottom: 2px solid #6aae6a;
+        display: inline-block;
+        margin-bottom: 30px;
+        padding: 0 5px;
+      }
+
+      .maincontent :global(h2) {
+        font-family: 'Open Sans', sans-serif;
+        font-size: 100%;
+        margin-bottom: .7rem;
+      }
+
+      .maincontent {
+        font-family: 'Open Sans', sans-serif;
+      }
+
+      .maincontent :global(a),
+      .maincontent :global(a:visited) {
+        text-decoration: none;
+        padding-bottom: 1px;
+        color: #1964ae;
+      }
+
+      .maincontent :global(a:hover) {
+        background-color: #ffb;
+      }
+
+      .maincontent :global(ul) {
+        font-size: 95%;
+      }
+
+    `}</style>
   </div>
 );
 
