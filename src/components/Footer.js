682e1a3f7732cf8f597c576bc6fde2a61df763b7
Richard Sternagel <rsternagel@gmail.com>
2017-05-19 12:26:29 +0200

    add responsive layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+// import FaGithub from 'react-icons/lib/fa/github'
+// import FaStackOverflow from 'react-icons/lib/fa/stack-overflow'
+
+import {s, m} from '../utils/breakpoints'
+
 const Footer = () => (
   <footer role="contentinfo">
     <div className="meta">
-      <span className="copyright">Copyright © { new Date().getFullYear() } <em>Richard Sternagel</em></span>
+      <span className="copyright">
+        © { new Date().getFullYear() }
+        <em>Richard Sternagel</em>
+      </span>
       {/*
       <Link to={'/prinzipien/'}>Prinzipien</Link>
       <span className="seperator apart"></span>
@@ -14,6 +22,13 @@ const Footer = () => (
         <span className="seperator">•</span>
         <Link to={'/datenschutz/'}>Datenschutz</Link>
       </span>
+      {/*
+      <span className="seperator apart"></span>
+      <span className="icons">
+        <a href="#"><FaGithub /></a>
+        <a href="#"><FaStackOverflow /></a>
+      </span>
+      */}
     </div>
 
     <style jsx>{`
@@ -32,9 +47,9 @@ const Footer = () => (
         border-bottom: 1px solid #b0dda4;
         background-color: #ebf6e8;
 
-        height: 45px;
         font-size: 80%;
         text-align: center;
+        padding: 8px 0;
       }
 
       footer :global(a),
@@ -50,9 +65,12 @@ const Footer = () => (
         background-color: #ffb;
       }
 
+      .icons {
+        font-size: 110%;
+      }
+
       .meta {
         margin: 0 auto;
-        padding-top: 9px;
         text-shadow: 0 1px 0 rgba(255,255,255, 1);
       }
 
@@ -62,7 +80,7 @@ const Footer = () => (
 
       .copyright em {
         font-style: normal;
-        padding-left: 10px;
+        padding-left: 5px;
       }
 
       .seperator.apart {
@@ -73,6 +91,13 @@ const Footer = () => (
       .seperator {
         padding: 0 5px 0 5px;
         color: #999;
+      }
+
+      /* Media Queries */
+
+      @media (max-width: ${s}) {
+        .copyright { display: block; }
+        .icons { display: block; }
       }
     `}</style>
   </footer>
