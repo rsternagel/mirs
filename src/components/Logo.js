be36c24b71d13e31b0b38c62a3f14748fd900650
Richard Sternagel <rsternagel@gmail.com>
2017-05-19 12:24:31 +0200

    add responsive logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,15 +5,19 @@ import logoMirs from './img/logo_mirs.png'
 const Logo = () => (
   <div className="logo">
     <Link to={'/'}>
-      <img src={logoMirs} alt="Medieninformatik Richard Sternagel" />
+      <img src={logoMirs} alt="Medieninformatik Richard Sternagel" width="214" height="58" />
     </Link>
 
     <style jsx>{`
-      /* TODO: replace with smaller img */
+      .logo {
+        width: 65%;
+      }
+
       .logo :global(img) {
+        min-width: 214px;
+        min-height: 58px;
         margin-left: -10px;
         margin-top: 5px;
-        width: 65%;
       }
     `}</style>
   </div>
