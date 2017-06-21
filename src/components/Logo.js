64ce71c5f657a5d18ef5173d9da5db0d3f2c4947
Richard Sternagel <rsternagel@gmail.com>
2017-06-21 17:56:42 +0200

    add new logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,24 +3,24 @@ import Link from 'gatsby-link'
 import logoMirs from './img/logo_mirs.png'
 
 const Logo = () => (
-  <div className="logo">
-    <Link to={'/'}>
-      <img src={logoMirs} alt="Medieninformatik Richard Sternagel" width="214" height="58" />
-    </Link>
+    <div className="logo">
+      <Link to={'/'}>
+        <img src={logoMirs} alt="Medieninformatik Richard Sternagel" width="216" height="58" />
+      </Link>
 
-    <style jsx>{`
-      .logo {
-        width: 65%;
-      }
+      <style jsx>{`
+        .logo {
+          width: 65%;
+        }
 
-      .logo :global(img) {
-        min-width: 214px;
-        min-height: 58px;
-        margin-left: -10px;
-        margin-top: 5px;
-      }
-    `}</style>
-  </div>
+        .logo :global(img) {
+          min-width: 216px;
+          min-height: 58px;
+          margin-left: -10px;
+          margin-top: 5px;
+        }
+      `}</style>
+    </div>
 )
 
 export default Logo
