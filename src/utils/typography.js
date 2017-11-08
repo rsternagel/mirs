38df3998b10bf42377d21d60415fbae857d05c5d
Richard Sternagel <rsternagel@gmail.com>
2017-11-09 00:37:25 +0100

    raise typography deps

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,8 @@
 import Typography from 'typography'
 
-const typography = new Typography()
+const typography = new Typography({
+  headerLineHeight: 1.45
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
