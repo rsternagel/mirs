e081c6bc386c433ddd9166b03b2e57ec84df04c1
Richard Sternagel <rsternagel@gmail.com>
2021-03-06 14:04:42 +0100

    raise deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint'
+    'prettier'
   ],
   plugins: ['@typescript-eslint', 'prettier'],
   parserOptions: {
